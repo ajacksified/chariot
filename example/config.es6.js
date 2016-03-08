f39078ebb5d6eb980d2e2634a91af6803bdd639e
Jack Lawson <ajacksified@gmail.com>
2016-03-08 11:37:16 -0800

    Move example to separate project

--- example/config.es6.js
+++
@@ -1,8 +0,0 @@
-// config.es6.js
-
-const config = {
-  debug: process.env.DEBUG || true,
-  assetPath: process.env.ASSET_PATH || '',
-};
-
-export default config;
--- example/controllers/base.es6.js
+++
@@ -1,8 +0,0 @@
-import ReactController from 'chariot/src/reactController';
-import DefaultLayout from '../views/layouts/default';
-import BaseLayout from '../views/layouts/base';
-
-export default class BaseController extends ReactController {
-  layout = DefaultLayout;
-  pageLayout = BaseLayout;
-}
